e90e1a37f0e2c198900503d645af481923d092f0
Juan Ignacio Coronel <phjocoronel@gmail.com>
2026-01-26 09:09:52 -0300

    Add Google Tag Manager integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -64,6 +65,8 @@ export default function RootLayout({
         </script>
         
         {children}
+
+        <GoogleTagManager gtmId="GTM-M24PTNZN" />
       </body>
     </html>
   );
